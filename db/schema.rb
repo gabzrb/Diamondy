e6406ae1248d2a33d5da36ae2e149adac3a1d09a
gabriel zerbib <gabrielzerbib6@gmail.com>
2019-05-04 13:43:02 +0300

    homepage sans video

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_04_14_162024) do
 
   create_table "products", force: :cascade do |t|
     t.text "description"
+    t.string "type"
+    t.string "photo"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -60,7 +62,6 @@ ActiveRecord::Schema.define(version: 2019_04_14_162024) do
     t.string "color"
     t.string "purity"
     t.string "certificate"
-    t.string "photo"
     t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -70,6 +71,10 @@ ActiveRecord::Schema.define(version: 2019_04_14_162024) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "photo"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

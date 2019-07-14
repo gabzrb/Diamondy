1b793a8eb10f34d16518172ec9a3554369edd0d0
David Sellam <dvdsellam@gmail.com>
2019-07-14 15:06:22 +0300

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_080047) do
+ActiveRecord::Schema.define(version: 2019_07_14_115312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2019_05_31_080047) do
     t.boolean "newsletter"
     t.boolean "offers"
     t.boolean "admin", default: false
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

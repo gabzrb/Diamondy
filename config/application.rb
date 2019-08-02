4f6d98796cb74f0421abf3bb7486e2be7b49d12b
David Sellam <dvdsellam@gmail.com>
2019-08-02 15:51:45 +0300

    moved a line

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Diamondy
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :fr
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -22,4 +23,3 @@ module Diamondy
     # the framework and any gems in your application.
   end
 end
-config.i18n.default_locale = :fr

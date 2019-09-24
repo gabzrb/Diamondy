b1689b4d3dcbc5d45bab801ec9bf7f4b860bdf94
David Sellam <dvdsellam@gmail.com>
2019-09-24 20:34:46 +0300

    model methods are done from specific searches on Stone class

--- app/models/stone.rb
+++ app/models/stone.rb
@@ -1,15 +1,46 @@
 class Stone < ApplicationRecord
-  SIZES = ['brillant(rond)', 'coussin', 'émeraude', 'radiant', 'baguette', 'coeur', 'ovale',  'marquise', 'princesse', 'poire', 'triangle']
+  # DIAMOND CLASS
+
+  SHAPES = ['brillant(rond)', 'coussin', 'émeraude', 'radiant', 'baguette', 'coeur', 'ovale',  'marquise', 'princesse', 'poire', 'triangle']
+  QUALIFICATIONS = ['Fair', 'Good', 'Very Good', 'Excellent']
   COLORS = ['D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'MZ']
   PURITY = ['IF-LC', 'VVS1', 'VVS2', 'VS1', 'VS2', 'SI1', 'SI2', 'P1', 'P2', 'P3']
   CERTIFICATES = ['HRD', 'GIA', 'LFG', 'Pas de certificat']
-
+  FLUO = ['None/Nil', 'Slight/Faint', 'Medium', 'Strong']
 
   belongs_to :product
-  validates :size, inclusion: { in: SIZES }
+  validates :size, inclusion: { in: QUALIFICATIONS }
   validates :color, inclusion: { in: COLORS }
   validates :purity, inclusion: { in: PURITY }
   validates :certificate, inclusion: { in: CERTIFICATES }
+  validates :symetry, inclusion: { in: QUALIFICATIONS }
+  validates :polish, inclusion: { in: QUALIFICATIONS }
+  validates :fluo, inclusion: { in: FLUO }
+  validates :shape, inclusion: { in: SHAPES }
 
+  def self.by_size(range)
+    Stone.all.select { |diamond| range.include? diamond.size }
+  end
 
+  def self.by_polish(range)
+    Stone.all.select { |diamond| range.include? diamond.polish }
+  end
+
+  def self.by_weight(min, max)
+    return [] if max < min
+
+    Stone.all.select { |diamond| (min..max).include? diamond.weight }
+  end
+
+  def self.by_color(range)
+    Stone.all.select { |diamond| range.include? diamond.color }
+  end
+
+  def self.by_purity(range)
+    Stone.all.select { |diamond| range.include? diamond.purity }
+  end
+
+  def self.by_symetry(range)
+    Stone.all.select { |diamond| range.include? diamond.symetry }
+  end
 end

5e54d8fedf27cb83955bfe08b631c6b58a380a72
David Sellam <dvdsellam@gmail.com>
2019-10-13 13:03:30 +0300

    change config algolia

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: ENV['Application_ID'], api_key: ENV['Search_Only_API_Key'] }
+AlgoliaSearch.configuration = { application_id: ENV['Algolia_Application_ID'], api_key: ENV['Algolia_Admin_API_Key'] }

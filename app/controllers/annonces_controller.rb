55d1be77544e6060afc42ab3aeed227ddbcce1d1
David Sellam <dvdsellam@gmail.com>
2019-10-27 18:53:06 +0200

    auto

--- app/controllers/annonces_controller.rb
+++ app/controllers/annonces_controller.rb
@@ -1,5 +1,5 @@
 class AnnoncesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :new]
   before_action :set_annonce, only: [:edit, :update, :destroy]
 
   def index

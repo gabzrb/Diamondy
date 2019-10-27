80c83125b3988318d75773ea6912923504fbe79c
David Sellam <dvdsellam@gmail.com>
2019-10-27 17:58:08 +0200

    small changes

--- app/controllers/jewels_controller.rb
+++ app/controllers/jewels_controller.rb
@@ -1,6 +1,7 @@
 class JewelsController < ApplicationController
   before_action :set_product, only: [:new, :create, :edit]
   before_action :set_jewel, only: [:show, :edit, :update]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:query].present?
--- app/controllers/special_requests_controller.rb
+++ app/controllers/special_requests_controller.rb
@@ -1,6 +1,7 @@
 class SpecialRequestsController < ApplicationController
   before_action :set_product, only: [:new, :create, :edit]
   before_action :set_special_request, only: [:show, :edit, :update]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:query].present?

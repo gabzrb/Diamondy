f2993f37cc1f3919858e48a6d54207b64abedf7d
gabriel zerbib <gabrielzerbib6@gmail.com>
2020-10-28 21:39:21 +0200

    addresse

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :siret_no, :services_description, :photo, :offers, :newsletter, :company_name, :phone, :service_description, :adress])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :siret_no, :services_description, :photo, :offers, :newsletter, :company_name, :phone, :service_description, :address])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :siret_no, :services_description, :photo, :offers, :newsletter, :company_name, :phone, :service_description, :adress])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :siret_no, :services_description, :photo, :offers, :newsletter, :company_name, :phone, :service_description, :address])
   end
 end
 

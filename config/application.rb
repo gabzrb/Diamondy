a62f1130014ff36428a8f09f795b8268012e3b33
gabriel zerbib <gabrielzerbib6@gmail.com>
2020-11-18 16:46:12 +0200

    edit + dashboard

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Diamondy
     config.i18n.default_locale = :fr
     config.assets.paths << "#{Rails.root}/app/assets/videos"
     config.assets.enabled = true
+    config.time_zone = 'UTC'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
